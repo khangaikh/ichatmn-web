a0909d51a5aea4f548b92ffd31cc1ffecd7bef06
khangai <khangai@alchemist.mn>
2016-04-04 11:19:10 +0900

    Some

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <?php
     $db_exists = file_exists("/home/bb/Public/db/ichat.db");
-    $db = new PDO('sqlite:host=/home/bb/Public/db/ichat.db');
+    $db = new PDO('sqlite:/home/bb/Public/db/ichat.db');
       
 ?>
