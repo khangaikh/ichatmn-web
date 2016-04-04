a941b541a4edc5c8e4920cdfe2379763dc70940c
khangai <khangai@alchemist.mn>
2016-04-04 11:10:25 +0900

    bug

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <?php
     $db_exists = file_exists("/home/bb/Public/db/ichat.db");
-    $db = new PDO('/home/bb/Public/db/sqlite:ichat.db');
+    $db = new PDO('/home/bb/Public/db/ichat.db');
       
 ?>
