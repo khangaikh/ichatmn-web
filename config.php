fd410b9a8177c1fb838ac37d7089ce7c191c42ee
khangai <khangai@alchemist.mn>
2016-05-13 13:42:11 +0900

    Some changes

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <?php
-    $db_exists = file_exists("/home/bb/Public/db/ichat.db");
-    $db = new PDO('sqlite:/home/bb/Public/db/ichat.db');
+    $db_exists = file_exists("ichat.db");
+    $db = new PDO('sqlite:ichat.db');
       
 ?>
